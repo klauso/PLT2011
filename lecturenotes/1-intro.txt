55ad3909c6a2c056627d526fd9bced755678061d
Klaus Ostermann <klaus612@gmail.com>
2011-10-16 18:50:50 +0200

    first two sets of lecture notes

---
+++ lecturenotes/1-intro.txt
@@ -0,0 +1,86 @@
+$Rev: 3058 $
+$LastChangedDate: 2011-10-16 17:27:43 +0200 (So, 16 Okt 2011) $.
+
+Introduction
+============
+
+These are lecture notes for the "Programming Languages and Types" at the University of Marburg
+
+loosely based on "Programming Languages: Application and Interpretation" by
+Shriram Krishnamurthi
+
+Please send comments or errors in these notes via email to Klaus Ostermann.
+My email address can be found on my webpage.
+
+
+Why are you here?
+=================
+
+This is an advanced programming course that focuses on the design and semantics
+of programming languages.
+
+The goals of this course are:
+
+* To change the way you solve problems in programming: By principled design,
+  with a rich toolbox of abstract concepts, rather than by tinkering.
+* Enable you to think beyond the currently fashionable programming languages.
+* Enable you to evaluate and discuss programming languages in a principled
+  way.
+* To show you where computer languages come from and potential future
+  directions.
+* To demonstrate why languages should be regarded as the ultimate form of
+  abstraction.
+* To teach you how to recognize abstractions and how to turn them into a
+  language design.
+* Teach you different ways how to realize a language design and make it
+  practically useful.
+* To convey a sense of aesthetics in programming.
+* To introduce you into one of the most beautiful theories in theoretical
+  computer science.
+* To turn you into a better programmer!
+
+Non-goals of this course are:
+
+* Learn X different programming languages. Rather, you'll learn to decompose a
+  PL into its features and discuss the merits of the features independently of the
+  concrete PL.
+* Write parsers or implement full-fledged compilers (although we will touch some
+  compiler issues).
+* Turn you into an evangelist for a particular PL.
+
+
+How will we get there?
+======================
+
+We will typically explain language features in two steps. First, we'll give an
+informal introduction and discuss some examples to get some experience with it.
+Then we will distill the core of the feature into an executable interpreter.
+Later, when we have more experience in formalizing language design into
+interpreters, we will also use formal mathematical notation.
+
+We will not just present the design of these features as if had disappeared out
+of the blue sky. Rather, we will discuss the process and trade-offs (and
+potential mistakes and pitfalls) that led to these designs. We will also
+discuss how these features appear in various available programming languages.
+
+We will mainly use the programming language Scala to write interpreters. Scala is
+sufficiently powerful to allow concise and elegant interpreters; on the other hand,
+it is sufficiently mature and popular for industrial usage. It also has variants
+of many of the features we will discuss. The book by Krishnamurthi, on which we loosely
+base the first part of the course, uses a different language, Racket. You are also
+welcome to use Racket instead of (or in addition to) Scala. Racket embodies
+an important tradition of PL design that is worth knowing, hence we encourage you,
+independently of this course, to study it!
+
+
+Topics for class discussion:
+============================
+
+* What would you like to learn in this course? What are your expectations?
+* Which programming languages do you know? Which features of these languages do you
+  like or dislike, and why?
+* How can we evaluate programming languages?
+* Is the choice of programming language for a programming language significant?
+* Are interpreters a good way to capture the meaning of a PL? How do they compare
+  to informal descriptions, formal specifications, and compilers?
+* How important is the syntax of a PL?
---
+++ lecturenotes/2-ae.scala
@@ -0,0 +1,189 @@
+// $Rev: 3058 $
+// $LastChangedDate: 2011-10-16 17:27:43 +0200 (So, 16 Okt 2011) $.
+
+/* 
+ * Introduction to Scala and interpreters
+ ****************************************
+ 
+ This version number of this document is $Rev: 3058 $ and 
+ it was last changed on $LastChangedDate: 2011-10-16 17:27:43 +0200 (So, 16 Okt 2011) $.
+
+ This file can be executed with the scala interactive interpreter.
+ Invoke with 'scala ae.scala'
+ */
+
+ 
+/* Case classes offer a convenient way to define 
+ * a data type with variants, as they occur typically
+ * in abstract syntax trees.
+ *
+ * Here is a simple example for a language that features
+ * arithmetic expressions with variables 
+ */
+
+ // The sealed keyword means that all subclasses must be defined
+// in this file. It enables completeness checks for pattern matches
+sealed abstract class Exp 
+case class Num(n: Int) extends Exp
+case class Add(lhs: Exp, rhs: Exp) extends Exp
+case class Mul(lhs: Exp, rhs: Exp) extends Exp
+case class Id(x: Symbol) extends Exp 
+// Symbols are similar to strings, but they are implicitly "canonicalized" and
+// can hence be compared efficiently. Unlike strings, they can not be manipulated.
+// They can be constructed by prefixing an identifier with a '
+
+/* Here is a sample program written in this language, directly written
+ * down using case class constructors */
+
+ val test0 = Add(Mul(Id('x),Num(2)),Add(Id('y),Id('y)))
+
+/* With a proper parser we could choose a syntax like "x*2+y+y".
+ * We do not care much about concrete syntax and parsing, though.
+ * That said, to make writing examples less verbose, Scala's implicits
+ * come to the rescue.
+ 
+ * Calls to implicit functions are inserted automatically by the compiler
+ * if they help to restore well-typedness. For instance, we can define: */
+ 
+implicit def num2exp(n: Int) = Num(n)
+implicit def sym2exp(x: Symbol) = Id(x)
+
+/* to lift integers and symbols to expressions. Using these implicits, the 
+ * example can be written as: */ 
+
+ val test = Add(Mul('x,2),Add('y,'y))
+
+/* To give meaning to identifiers, we use _environments_. Environments are
+ * mappings from Identifiers (which we represent as symbols) to Values.
+ * In our simple language the only values are integers, hence: */
+type Env = Map[Symbol,Int]
+
+/* An evaluator (or interpreter) for this language takes an expression and
+ * an environment as parameter and produces a value - in this case "Int".
+ * 
+ * The interpreter illustrates how pattern matching over case classes works.
+ */
+def eval(e: Exp, env: Env) : Int = e match {
+  case Num(n) => n
+  case Id(x) => env(x)
+  case Add(l,r) => eval(l,env) + eval(r,env)
+  case Mul(l,r) => eval(l,env) * eval(r,env)
+}
+/* A different (and arguably more 'object-oriented') way to implement this
+ * evaluator would be to add an abstract "eval" method to the Exp class and
+ * override it in all subclasses, each implementation corresponding to its
+ * corresponding case in the pattern match. The choice between these alternatives
+ * matters, since they support different dimensions of extensibility.
+ *
+ * We will mainly use the more functional style using pattern matching, because
+ * it matches better to the order in which we present these topics in the lecture.
+ *
+ * To try the example, we need a sample environment that gives values to the 
+ * (free) variables in the sample expression. The test environment also illustrates
+ * how Scala supports direct definitions of constant maps.
+ */
+
+val testEnv = Map('x -> 3, 'y -> 4)
+
+/* We can automatically test our evaluator using assert : */
+assert( eval(test, testEnv) == 14)
+
+/* We will now learn a different way to encode algorithms that operate on 
+ * expressions (like the evaluator).
+ * To this end, we will now use so-called "folds". Folds are well-known
+ * for lists, but the concept is more general and applies to arbitrary
+ * algebraic data types.
+ *
+ * We will present folds in such a style that they resemble visitors as known
+ * from the OO design pattern literature. They correspond to so-called
+ * "internal visitors" in which the traversal is encoded within the "accept" function.
+ 
+ * An internal visitor consists of one function for each syntactic construct of the
+   language. It has a type parameter that determines the "return type" of invoking the
+   visitor on an expression. This type parameter is used in all positions in which
+   the original syntax specifies a subexpression.
+   
+ * Internal visitors also correspond to a "bottom-up" traversal of the syntax tree. */ 
+
+case class Visitor[T](num: Int => T, add: (T,T)=>T, mul: (T,T)=>T, id: Symbol=>T)
+
+/* The fold function itself applies a visitor to an expression. 
+ * Note that the recursion is performed in the fold function, hence
+ * all visitors are not recursive.
+ *
+ * Also note that this design enforces that all algorithms specified via this visitor
+ * interfaces are compositional by design. This means that the recursion structure of
+ * the algorithm corresponds to the recursion structure of the expression. */
+ 
+def foldExp[T](v: Visitor[T], e: Exp) : T = {
+  e match {
+    case Num(n) => v.num(n)
+    case Id(x) => v.id(x)
+    case Add(l,r) => v.add(foldExp(v,l),foldExp(v,r))
+    case Mul(l,r) => v.mul(foldExp(v,l),foldExp(v,r))
+  }
+}
+/* Here is our evaluator from above rephrased using the visitor infrastructure. */
+
+val evalVisitor = Visitor[Env=>Int]( env=>_, (a,b)=>env=>a(env)+b(env), (a,b)=>env=>a(env)*b(env), x=>env => env(x)) 
+
+/* We can of course also restore the original interface of eval */
+
+def eval2(e: Exp) = foldExp[Env=>Int](evalVisitor,e)
+
+/* Let's test whether it works. */
+
+assert( eval2(test)(testEnv) == 14)
+
+/* We can of course also apply other algorithms using visitors, such as 
+ * counting the number of "Num" literals, or printing to a string: */
+val countVisitor = Visitor[Int]( _=>1, _+_, _+_, _=>0) 
+val printVisitor = Visitor[String](_.n.toString, "("+_+"+"+_+")",_+"*"+_,_.x.toString)
+
+def countNums(e: Exp) = foldExp[Int](countVisitor,e) 
+
+assert(countNums(test) == 1)
+
+/* We will now learn a different "data-less" representation of expressions inspired by
+ * the visitor infrastructure above. This representation is called "Church Encoding"
+ * or sometimes, when used in statically typed languages, "Böhm-Berarducci Encoding".
+ *
+ * The idea of this representation is that the expression tree is a kind of intermediate
+ * result that we can skip and go straight to the result of applying a visitor. That is,
+ * we represent an expression by a function that will call the "right" functions of a visitor.
+ *
+ * An expression is represented by a function that, for any type T, applies a Visitor[T] to
+ * itself and thereby produces a T.
+ * 
+ * The "for any type T" from the previous sentence means that ordinary Scala functions cannot
+ * be used for this purpose. Rather, we encode it as an abstract class with an "apply" function
+ * that takes the type parameter.
+ * 
+ * Later in the course, or if you are a Haskell affectionado, you may recognize that this is
+ * an encoding of the type "forall T.Visitor[T] => T", which means that functions accepting or 
+ * returning ExpC values are so-called "rank-2 types".
+ */
+
+abstract class ExpC { def apply[T](v: Visitor[T]) : T } // we call this method "apply" because then 
+                                                        // we can use function application syntax
+
+implicit def num(n: Int) : ExpC = new ExpC {  // we use implicits for num and id again to make 
+     def apply[T](v: Visitor[T]) = v.num(n) } // building expressions more concise.
+implicit def id(x: Symbol) : ExpC = new ExpC {
+     def apply[T](v: Visitor[T]) = v.id(x) }
+def add(l: ExpC, r:ExpC) : ExpC = new ExpC {
+     def apply[T](v: Visitor[T]) = v.add(l(v),r(v)) } // note the indirect recursion here!
+def mul(l: ExpC, r:ExpC) : ExpC = new ExpC {
+     def apply[T](v: Visitor[T]) = v.mul(l(v),r(v)) } // note the indirect recursion here!
+
+
+/* We can again reconstruct the original eval interface. Note that the Visitor is 
+  *_applied_ to the expression. */
+def eval3(e: ExpC, env: Env) : Int = e(evalVisitor)(env)
+     
+// example test from above rewritten into Church encoding
+val test2 : ExpC = add(mul('x,2),add('y,'y))
+  
+assert(eval3(test2, testEnv) == 14)
+
+assert(test2(countVisitor) == 1)
